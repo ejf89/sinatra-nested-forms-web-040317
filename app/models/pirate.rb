2ec6a44e906c1117585a166d98e9d03efb06931a
github username <github email address>
2017-04-26 13:06:35 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+
+    ALL = []
+
+    def self.all
+        ALL
+    end
+
+    def initialize(params)
+        @name = params[:name]
+        @weight = params[:weight]
+        @height = params[:height ]
+        ALL << self
+    end
+
 end
